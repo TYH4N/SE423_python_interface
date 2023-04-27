03d9199f9304d5fe68825c8d0fea90e218b1d71f
TYH4N <tianyih4@outlook.com>
2023-04-27 16:45:53 -0500

    [SW] Tidy up and adding parameters changing textbox

--- python_gui/tcpClient.py
+++ python_gui/tcpClient.py
@@ -1,90 +1,100 @@
-#!/usr/bin/env python
-
 import socket
 from time import sleep
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from matplotlib.widgets import RadioButtons, Button
+from matplotlib.widgets import RadioButtons, Button, TextBox
 import numpy as np
 
+# Setting server address
 TCP_IP = '192.168.1.69'
 TCP_PORT = 10001
 BUFFER_SIZE = 1024
-MESSAGE = '123'
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.connect((TCP_IP, TCP_PORT))
 
-# while True:
-#     s.send(MESSAGE.encode())
-#     data = s.recv(BUFFER_SIZE).decode().split(' ')
-#     x = data[0]
-#     y = data[1]
-#     theta = data[3]
-#     print(x,y,theta)
-#     sleep(0.2)
-# s.close()
-# print ("received data:", data)
+# Initialize variables
+stop_flag = False
+clear_flag = False
+refresh_interval = 100 # [ms]
+Kp, Kd = 0, 0
 
-fig, ax = plt.subplots(figsize = [9,20])
-plt.subplots_adjust(bottom=0.2)
-plt.xlim(-8.2, 8.2)
-plt.ylim(-0.2, 16.2)
-ax.set_xticks(np.arange(-8,9,1))
-ax.set_yticks(np.arange(0,17,1))
+# Initialize plot
+fig, ax = plt.subplots(figsize = [8,7.5])
+plt.subplots_adjust(bottom = 0.1, right = 0.7)
+plt.xlim(-7.0, 7.0)
+plt.ylim(-5.0, 13.0)
+ax.set_xticks(np.arange(-7,8,1))
+ax.set_yticks(np.arange(-5,14,1))
 ax.grid()
 points = []
 line, = ax.plot(points, '.')
 
-color_rax = plt.axes([0.05, 0.5, 0.1, 0.15])
-color = 'blue'
-rax = RadioButtons(color_rax, ('blue', 'green', 'red'))
+# Initialize map
+lx, ly = [2, 6, 6, -6, -6, -2], [0, 0, 12, 12, 0, 0]
+plt.plot(lx, ly, color = 'brown')
+circle1 = plt.Circle((-2, -4), 1, color = 'blue', fill = False)
+circle2 = plt.Circle((2, -4), 1, color = 'orange', fill = False)
+ax.add_patch(circle1)
+ax.add_patch(circle2)
+ 
+# Kp TextBox
+input_box_1 = plt.axes([0.85, 0.7, 0.15, 0.05])
+txt_box_1 = TextBox(input_box_1, "Kp ", color = 'white', hovercolor = '.9')
+def change_value(data):
+    Kp = data
+    print("Kp changed to", Kp)
+txt_box_1.on_submit(change_value)
+txt_box_1.set_val(' ')
 
-def update_color(label):
-    global color
-    color = label
-rax.on_clicked(update_color)
+# Kd TextBox
+input_box_2 = plt.axes([0.85, 0.65, 0.15, 0.05])
+txt_box_2 = TextBox(input_box_2, "Kd ",  color = 'white', hovercolor = '.9')
+def change_value(data):
+    Kd = data
+    print("Kd changed to", Kd)
+txt_box_2.on_submit(change_value)
+txt_box_2.set_val(' ')
 
-clear_ax = plt.axes([0.8, 0.9, 0.1, 0.075])
-clear_button = Button(clear_ax, 'Clear')
-
+# Define clear widget
+clear_ax = plt.axes([0.8, 0.9, 0.1, 0.1])
+clear_button = Button(clear_ax, 'Clear', color = 'white', hovercolor = '.8')
 def clear_points(event):
     global points
     points = []
     line.set_data(list(zip(*points)))
 clear_button.on_clicked(clear_points)
 
-stop_ax = plt.axes([0.9, 0.9, 0.1, 0.075])
-stop_button = Button(stop_ax, 'Stop')
-
-stop_flag = True
-clear_flag = False
+# Define stop widget
+stop_ax = plt.axes([0.9, 0.9, 0.1, 0.1])
+stop_button = Button(stop_ax, 'Stop', color = 'white', hovercolor = '.8')
 def stop_receiving(event):
     global stop_flag
     stop_flag = not stop_flag
     clear_flag = True
 stop_button.on_clicked(stop_receiving)
 
+# Update thread
 def update_plot(frame):
     x = 0
     y = 0
+    tx_data = [0, 0, 0, 0, 0, 0, 0, 0]
     try:
-        s.send(MESSAGE.encode())
-        data = s.recv(BUFFER_SIZE).decode().split('\r\n')
-        data = data[0].split(' ')
-        print(data)
-        if (len(data) > 4):
-            x = data[0]
-            y = data[1]
-            theta = data[3]
-            if stop_flag and data:
+        server.send(tx_data.encode())
+        rx_data = server.recv(BUFFER_SIZE).decode().split('\r\n')
+        rx_data = rx_data[0].split(' ')
+        print(rx_data)
+        if (len(rx_data) > 4):
+            x = rx_data[0]
+            y = rx_data[1]
+            theta = rx_data[3]
+            if (not stop_flag):
                 point = [float(x), float(y)]
-                if not clear_flag:
+                if (not clear_flag):
                     points.append(point)
                     line.set_data(list(zip(*points)))
                     line.set_color(color)
     except socket.timeout:
         pass
 
-ani = FuncAnimation(fig, update_plot, frames=None, repeat=True, blit=False, interval=100)
+animation = FuncAnimation(fig, update_plot, frames=None, repeat=True, blit=False, interval=refresh_interval)
 plt.show()
-
